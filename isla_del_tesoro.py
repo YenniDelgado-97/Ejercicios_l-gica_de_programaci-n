d69458b408621a19f3099fd1c9abf493d6bfa840
Yenni Vanessa Delgado <yennivanessadelgado@gmail.com>
2022-10-31 12:18:01 -0500

    actualización de los ejercicios

--- isla_del_tesoro.py
+++ isla_del_tesoro.py
@@ -1,21 +1,21 @@
 print("Bienvenido a la isla, tu misión será encontrar el tesoro")
 print("Para iniciar la misión los ancestros de estas tierras te han ofrecido un mapa")
-print("este mapa tiene 2 caminos que te conduciran a la victoria o a la muerte. Qué inicie está aventura ")
+print("este mapa tiene 2 caminos que te conduciran a la victoria o a la muerte. ¡Qué inicie está aventura! ")
 
-camino=input("por favor selecciona un camino, escribe derecha o izquierda?")
+camino=input("por favor selecciona un camino, escribe la palabra derecha o izquierda para seleccionar el camino que desees seguir?")
 
 if camino == "derecha":
     print("siento algunos ruidos, decide rapidamente")
-    seleccion= input("te agachas o saltas?")
+    seleccion= input("te agachas o saltas?.Escribe la palabra agacharse o saltar")
     if seleccion == "agacharse":
         print("en el suelo hay dos flores que te daran un poder, elige sabiamente")
-        flor=input("roja o azul")
+        flor=input("roja o azul. Escribe la palabra roja o azul para seleccionar la rosa")
         if flor == "azul":
             print("recibiste un poder secreto")
-            aceptar = input("lo aceptos o no")
+            aceptar = input("lo aceptos o no. Escribe la palabra si o no")
             if aceptar == "no":
                 print("encuentras una cueva")
-                entrar= input("deseas entrar o no")
+                entrar= input("deseas entrar o no. Escribe la palabra si o no")
                 if entrar == "si":
                     print("ganaste, encontraste e tesoro")
                 elif entrar == "no":
